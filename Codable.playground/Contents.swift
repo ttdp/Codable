c74713eab5272310cd023a41323ced5469490e60
ttdp <tiantong.dev@gmail.com>
2020-04-29 14:22:10 +0800

    add demo

--- Codable.playground/Contents.swift
+++ Codable.playground/Contents.swift
@@ -31,41 +31,41 @@ let country = try JSONDecoder().decode(Country.self, from: jsonData)
 print(country.name, country.language, country.continent)
 // China Chinese Asia
 
+let encoded = try JSONEncoder().encode(country)
+print(String(data: encoded, encoding: .utf8)!)
+// {"name":"China","continent":"Asia","language":"Chinese"}
+
 // MARK: - Struct properties and json keys are not same
 
 struct Country2: Codable {
-    let nationName: String
-    let speakingLanguage: String
+    let name: String
+    let language: String
     let continent: Continent
     
     enum CodingKeys: String, CodingKey {
-        case nationName = "name"
-        case speakingLanguage = "language"
+        case name = "country_name"
+        case language = "country_language"
         case continent
     }
 }
 
 let json2 = """
 {
-    "name": "China",
-    "language": "Chinese",
-    "continent": "Asia",
-    "capital": {
-        "name": "Beijing",
-        "population": 20000000
-    }
+    "country_name": "China",
+    "country_language": "Chinese",
+    "continent": "Asia"
 }
 """
 let jsonData2 = json2.data(using: .utf8)!
 
 let country2 = try JSONDecoder().decode(Country2.self, from: jsonData2)
-print(country2.nationName, country2.speakingLanguage, country2.continent)
+print(country2.name, country2.language, country2.continent)
 // China Chinese Asia
 
 // MARK: - Nested Struct
 
 struct Capital: Codable {
-    let name: String
+    let city: String
     let population: Int
 }
 
@@ -82,7 +82,7 @@ let json3 = """
     "language": "Chinese",
     "continent": "Asia",
     "capital": {
-        "name": "Beijing",
+        "city": "Beijing",
         "population": 20000000
     }
 }
@@ -91,11 +91,12 @@ let jsonData3 = json3.data(using: .utf8)!
 
 let country3 = try JSONDecoder().decode(Country3.self, from: jsonData3)
 print(country3.name, country3.language, country3.continent, country3.capital)
-// China Chinese Asia Capital(name: "Beijing", population: 20000000)
+// China Chinese Asia Capital(city: "Beijing", population: 20000000)
 
 // MARK: - Struct Array
 let json4 = """
-[   {
+[
+    {
         "name": "China",
         "language": "Chinese",
         "continent": "Asia"
